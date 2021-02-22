aa995082e7c40f66b13316cdb4e552d7055b287a
noviannurrohman <noviannurrohman99@gmail.com>
2021-02-22 22:00:15 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const Hello = ()=>{
-	return <p>Hello</p>
-}
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
